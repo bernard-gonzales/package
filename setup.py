fe2d5b378e76dcad814cc77458f43226e2bc4c98
Bernard Gonzales <bernardgonzales125@gmail.com>
2024-07-02 11:03:45 -0400

    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='package',
+    version='0.1',
+    description='A small package',
+    url='https://github.com/bernard-gonzales/package',
+    author='Bernard Gonzales',
+    author_email='bernardgonzales125@gmail.com',
+    license='MIT',
+    packages=['package']
+)
